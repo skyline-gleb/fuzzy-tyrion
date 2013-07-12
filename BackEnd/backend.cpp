7226e69608c902937bf66f8d6e613db609de9733
user <user@pc.(none)>
2013-07-12 11:45:08 +0700

    changed StringGenerate function

--- BackEnd/backend.cpp
+++ BackEnd/backend.cpp
@@ -1,9 +1,19 @@
 #include "DataGenerator.h"
 
 
-extern "C"
-char * stringGenerate(char *_s) {
-    
-    DataGenerator gen;
-    return (gen.randomlyGenerateData(_s);)
+extern "C" {
+
+    char * StringGenerate(const int _length) {
+
+        DataGenerator gen;
+        std::string str = (gen.randomlyGenerateString(_length));
+        char* pres;
+        pres = new char[str.size() + 1];
+        int i;
+        for (i = 0; i < str.size(); ++i)
+            pres[i] = str[i];
+        pres[i] = 0;
+        
+        return pres;
+    }
 }
