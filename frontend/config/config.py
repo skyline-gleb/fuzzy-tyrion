2b458280d8ebdb79965990e8d20c7ec2a01f8cbe
admin <ubuntu@ubuntu.(none)>
2013-07-15 07:46:04 +0400

    fixed config.py

--- frontend/config/config.py
+++ frontend/config/config.py
@@ -3,9 +3,6 @@ from configvalidator import *
 
 
 class Config(object):
-
-<<<<<<< HEAD
-	@staticmethod
 	def load(filename):
 		with open(filename, 'r') as config_file:
 			data = json.loads(config_file.read())
@@ -14,11 +11,3 @@ class Config(object):
 			print("Error")
 		for item in data:
 			setattr(Config, item, data[item])
-=======
-    @staticmethod
-    def load(filename):
-        with open(filename, 'r') as config_file:
-            data = json.loads(config_file.read())
-        for item in data:
-            setattr(Config, item, data[item])
->>>>>>> f25ea69f27d32c15416f3b024fea1528713921dc
