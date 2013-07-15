16f96a994782a2b75a94d68b556ec46764052600
admin <ubuntu@ubuntu.(none)>
2013-07-15 07:21:59 +0400

    added config validator

--- frontend/config/config.py
+++ frontend/config/config.py
@@ -1,11 +1,24 @@
 import json
+from configvalidator import *
 
 
 class Config(object):
 
+<<<<<<< HEAD
+	@staticmethod
+	def load(filename):
+		with open(filename, 'r') as config_file:
+			data = json.loads(config_file.read())
+		obj = Check(data)
+		if obj.Run() == -1:
+			print("Error")
+		for item in data:
+			setattr(Config, item, data[item])
+=======
     @staticmethod
     def load(filename):
         with open(filename, 'r') as config_file:
             data = json.loads(config_file.read())
         for item in data:
             setattr(Config, item, data[item])
+>>>>>>> f25ea69f27d32c15416f3b024fea1528713921dc
---
+++ frontend/config/configvalidator.py
@@ -0,0 +1,65 @@
+import json
+import codecs
+
+class Check:
+    def __init__(self,temp):
+        self.dictone = temp
+        self.count1 = 5
+        self.checkdict = {0:{"name": "api_url","type": str}}
+        self.checkdict[1] = {"name": "rps","type": int}
+        self.checkdict[2] = {"name": "headers","type": dict}
+        self.checkdict[3] = {"name": "parameters","type": dict}
+        self.checkdict[4] = {"name": "methods","type": dict}
+        self.count2 = 3
+        self.checkmethods = {0:{"name": "http_method", "type": str}}
+        self.checkmethods[1] = {"name": "count", "type": int}
+        self.checkmethods[2] = {"name": "parameters", "type": dict}
+        self.count3 = 4
+        self.checkparameters = {0:{"name": "required", "type": bool}}
+        self.checkparameters[1] = {"name": "data_type", "type": str}
+        self.checkparameters[2] = {"name": "value", "type": str}
+        self.checkparameters[3] = {"name": "action", "type": str}        
+    def Run(self):
+        line = self.dictone.keys()
+        i=0
+        while i<self.count1:
+            value = self.dictone.get(self.checkdict[i]["name"],-1)
+            if value == -1:
+                print("Error key level_1")
+                return -1
+            else:
+                if type(value) != self.checkdict[i]["type"]:
+                    print("Error value level_1")
+                    print(type(value))
+                    print(value)
+                    print(self.checkdict[i]["type"])
+                    return -1
+            i=i+1
+        line = self.dictone["methods"].keys()
+        for element in line:
+            i=0
+            while i<self.count2:
+                value = self.dictone["methods"][element].get(self.checkmethods[i]["name"],-1)
+                if value == -1:
+                    print("Error key level_2")
+                    return -1
+                else:
+                    if type(value) != self.checkmethods[i]["type"]:
+                        print("Error value level_2")
+                        return -1
+                parameters = self.dictone["methods"][element]["parameters"].keys()
+                for par_el in parameters:
+                    j=0
+                    while j<self.count3:
+                        value = self.dictone["methods"][element]["parameters"][par_el].get(self.checkparameters[j]["name"],-1)
+                        if value == -1:
+                            print("Error key level_3")
+                            return -1
+                        else:
+                            if type(value) != self.checkparameters[j]["type"]:
+                                print("Error value level_3")
+                                return -1
+                        j=j+1
+                        if j==2:
+                            j=j+1
+                i=i+1
