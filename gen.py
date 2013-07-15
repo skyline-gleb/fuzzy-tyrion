958bf0b196c61c35c4e8296032694acecc68c99d
Ivan Vologdin <vologdin.nsk@gmail.com>
2013-07-15 09:14:02 +0700

    fixed gen.py

--- gen.py
+++ gen.py
@@ -3,13 +3,13 @@ from ctypes import *
 
 class DataGenerator:
 	def __init__(self):
-		lib = cdll.LoadLibrary('./libBackEnd.so')
-		lib.generateString.restype = c_char_p
-		lib.generateString.argtypes = [c_int]
-		lib.generateInt32.restype = c_int
-		lib.generateInt32.argtypes = [c_int, c_int]
+		self.lib = cdll.LoadLibrary('./libBackEnd.so')
+		self.lib.generateString.restype = c_char_p
+		self.lib.generateString.argtypes = [c_int]
+		self.lib.generateInt32.restype = c_int
+		self.lib.generateInt32.argtypes = [c_int, c_int]
 	def getString(self, length = 10):
-		return lib.generateString(length)
+		return self.lib.generateString(length)
 	def getInteger32(self, minInt = 0, maxInt = 1000):
-		return lib.generateInt32(minInt, maxInt)
+		return self.lib.generateInt32(minInt, maxInt)
 
