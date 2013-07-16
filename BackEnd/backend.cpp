14240bd52c72a8b5391af6bb925141b6f616961c
dimocrat <dimocrat92@gmail.com>
2013-07-16 13:04:18 +0700

    added wrap for the function randomlyGenerateNumber

--- BackEnd/backend.cpp
+++ BackEnd/backend.cpp
@@ -18,55 +18,55 @@ extern "C" {
         return pres;
     }
     
-    short generate_short(const short _min = 0, const short _max = 1000){
+    short generate_short(const short _min = SHRT_MIN, const short _max = SHRT_MAX){
         DataGenerator gen;
         short res = gen.randomlyGenerateNumber<short>(_min, _max);
         return res;
     }
     
-    ushort generate_unsigned_short(const ushort _min = 0, const ushort _max = 1000){
+    ushort generate_ushort(const ushort _min = 0, const ushort _max = USHRT_MAX){
         DataGenerator gen;
         ushort res = gen.randomlyGenerateNumber<ushort>(_min, _max);
         return res;
     }
     
-    int generate_int32(const int _min = 0, const int _max = 1000){
+    int generate_int32(const int _min = INT_MIN, const int _max = INT_MIN){
         DataGenerator gen;
         int res = gen.randomlyGenerateNumber<int>(_min, _max);
         return res;
     }
     
-    uint generate_unsigned_int32(const uint _min = 0, const uint _max = 1000){
+    uint generate_uint32(const uint _min = 0, const uint _max = UINT_MAX){
         DataGenerator gen;
         uint res = gen.randomlyGenerateNumber<uint>(_min, _max);
         return res;
     }
     
     
-    long long generate_int64(const long long _min = 0, const long long _max = 1000){
+    long long generate_int64(const long long _min = LLONG_MIN, const long long _max = LLONG_MAX){
         DataGenerator gen;
         long long res = gen.randomlyGenerateNumber<long long>(_min, _max);
         return res;
     }
-    unsigned long long generate_unsigned_int64(const unsigned long long _min = 0, const unsigned long long _max = 1000){
+    unsigned long long generate_uint64(const unsigned long long _min = 0, const unsigned long long _max = ULLONG_MAX){
         DataGenerator gen;
         unsigned long long res = gen.randomlyGenerateNumber<unsigned long long>(_min, _max);
         return res;
     }
     
-    float generate_float(const float _min = 0, const float _max = 1000){
+    float generate_float(const float _min = FLT_MIN, const float _max = FLT_MAX){
         DataGenerator gen;
         float res = gen.randomlyGenerateNumber<float>(_min, _max);
         return res;
     }
     
-    double generate_double(const double _min = 0, const double _max = 1000){
+    double generate_double(const double _min = DBL_MIN, const double _max = DBL_MAX){
         DataGenerator gen;
         double res = gen.randomlyGenerateNumber<double>(_min, _max);
         return res;
     }
     
-    long double generate_long_double(const long double _min = 0, const long double _max = 1000){
+    long double generate_long_double(const long double _min = LDBL_MIN, const long double _max = LDBL_MAX){
         DataGenerator gen;
         long double res = gen.randomlyGenerateNumber<long double>(_min, _max);
         return res;
