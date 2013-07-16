b06a8da9f75ab196e11d821c9c47390017202835
Tsvetkov Nikita <tsvetkov.nikita@hotmail.com>
2013-07-16 09:26:13 +0700

    added fuzzer tests

--- frontend/tests/fuzzertest.py
+++ frontend/tests/fuzzertest.py
@@ -1,11 +1,13 @@
 from frontend import Fuzzer
+from frontend.http.request import Request
 import unittest
 
 
 class FuzzerTestCase(unittest.TestCase):
+    
     @classmethod
     def setUpClass(cls):
-        cls.fuzzer = Fuzzer('url')
+        cls.fuzzer = Fuzzer('http://api.localhost/v{version}/')
 
     def test_prepare_data_generate(self):
         parameters = {'where': {'action': 'generate', 'required': True, 'value': 'Novosibirsk', 'data_type': 'string'}}
@@ -18,6 +20,27 @@ class FuzzerTestCase(unittest.TestCase):
         data = self.fuzzer.prepare_data(parameters)
         self.assertNotIsInstance(data['where'], int)
         self.assertIsInstance(data['test'], int)
+
+    def test_make_url(self):
+        expected_url = 'http://api.localhost/v1/categories/cat1/items/1234'
+        api_method = 'categories/{category}/items/{item_id}'
+        payload = {'version': '1', 'category': 'cat1', 'item_id': 1234}
+        actual_url = self.fuzzer.make_url(self.fuzzer.api_url, api_method, payload)
+        self.assertEqual(actual_url, expected_url)
+
+    def test_make_request(self):
+        http_method = 'GET'
+        url = 'http://api.localhost/v1/users/1'
+        payload = {'fields': 'username, email'}
+        headers = {'Accept': 'application/json'}
+        
+        expected_request = Request(http_method, url)
+        expected_request.add_headers(headers)
+        expected_request.add_params(payload)
+
+        actual_request = self.fuzzer.make_request(http_method, url, payload, headers)
+        
+        self.assertEqual(vars(actual_request), vars(expected_request))
 
 
 if __name__ == '__main__':
