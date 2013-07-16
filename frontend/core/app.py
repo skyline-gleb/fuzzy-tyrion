b4c0da621413f69ff613ed25797d8c2c01fdd09f
Tsvetkov Nikita <tsvetkov.nikita@hotmail.com>
2013-07-16 09:40:09 +0700

    improved arguments parser

--- frontend/core/app.py
+++ frontend/core/app.py
@@ -4,13 +4,14 @@ import argparse
 class App(object):
 
     @staticmethod
-    def get_args():
+    def init():
+        App.args = App.parse_args()
+    
+    @staticmethod
+    def parse_args():
         parser = argparse.ArgumentParser(prog="fuzzer", description="HTTP API fuzzer")
         parser.add_argument("-c", "--config", help="set configuration file", required=True)
-        parser.add_argument("-r", "--report", help="set report file")
+        parser.add_argument("-r", "--report", help="set report file", required=True)
+        parser.add_argument("-l", "--log", help="set log file", required=True)
         parser.add_argument("-v", "--verbose", help="turn on verbose logging", action="store_true")
         return parser.parse_args()
-
-    @staticmethod
-    def init():
-        App.args = App.get_args()
