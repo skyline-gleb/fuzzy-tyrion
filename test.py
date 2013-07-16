012d5f892438f713c838830e9e53d6f30b82afaf
Ildar Khabatulin <psyhister@gmail.com>
2013-07-16 11:58:51 +0700

    added test method for unicode generator

--- test.py
+++ test.py
@@ -9,10 +9,13 @@ class TestSequenceFunctions(unittest.TestCase):
 		self.min = -100000000
 		self.max = 100000000
 		self.generator = DataGenerator()
-	def test_length(self):
+	def testStringLength(self):
 		string = self.generator.getString(self.length)
 		self.assertEqual(self.length, len(string))
-	def test_range(self):
+	def testUnicodeStringLength(self):
+		wstring = self.generator.getUnicodeString(self.length)
+		self.assertEqual(self.length, len(wstring))
+	def testRange(self):
 		integer = self.generator.getInteger32(self.min, self.max)
 		self.assertEqual((self.min < integer), (integer < self.max)) 
 if __name__ == '__main__':
