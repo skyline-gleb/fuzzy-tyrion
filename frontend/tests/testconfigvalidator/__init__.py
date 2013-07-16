168e6705346ce7a97f99cb6eec668ca208ba5f21
admin <ubuntu@ubuntu.(none)>
2013-07-16 11:30:46 +0700

    added test configvalidator

---
+++ frontend/tests/testconfigvalidator/__init__.py
@@ -0,0 +1,1 @@
+from .testconfigvalidator import TestCheck
--- runtests.py
+++ runtests.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 from frontend.tests import FuzzerTestCase
+from frontend.tests.testconfigvalidator import TestCheck
 import unittest
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(FuzzerTestCase))
+    suite.addTest(unittest.makeSuite(TestCheck))
     return suite
 
 if __name__ == '__main__':
